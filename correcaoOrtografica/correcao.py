cf76d11efbb13a0e20e0ba87004ae01a06e0682b
Patrick Ferreira Campos <patrick.campos55@gmail.com>
2020-10-26 15:24:27 -0300

    projeto de corretor ortografico finalizado

--- correcaoOrtografica/correcao.py
+++ correcaoOrtografica/correcao.py
@@ -1,9 +1,27 @@
 import nltk
 
+listaPalavras=[]
+f = open('/Users/Patrick F/Desktop/TesteChatBot/Aplicação/correcaoOrtografica/dicionario.txt', 'r', encoding='utf8')
+tokens = nltk.tokenize.word_tokenize(f.read())
+for token in tokens:
+    if token.isalpha():
+        listaPalavras.append(token.lower())
+
+
 def principal(texto):
-    lista = tratamento(texto) 
-    listaPalavras = geradorPalavras(lista) 
-    palavra_certa = max(listaPalavras,key=probabilidade)  
+    lista = tratamento(texto)
+    listaPalavras = []
+    palavra_certa = []
+    for palavra in lista:
+        listaPalavras += geradorPalavras(palavra)
+        #print("lista de palavras = {}\n".format(listaPalavras))
+        palavra_certa.append(max(listaPalavras,key=probabilidade))
+        listaPalavras = []
+    print(palavra_certa)
+        #palavra_certa = []
+    #print(listaPalavras)
+        #palavra_certa = max(palavra,key=probabilidade)
+    #palavra_certa = max(listaPalavras,key=probabilidade)
 
 def tratamento(texto):
     listaPalavras=[]
@@ -13,15 +31,16 @@ def tratamento(texto):
             listaPalavras.append(token.lower())
     return listaPalavras
 
-def geradorPalavras(palavras):
-    possibilidades = []
-    for palavra in palavras:
-        fatias = []
-        for i in range(len(palavra)):
-            fatias.append((palavra[:i],palavra[i:]))
-        possibilidades.append(inserirLetra(fatias))
+def geradorPalavras(palavra):
+    fatias = []
+    for i in range(len(palavra)+1):
+        fatias.append((palavra[:i],palavra[i:]))
+    possibilidades = inserirLetra(fatias)
+    possibilidades += deletandoLetra(fatias)
+    possibilidades += trocaLetra(fatias)
+    possibilidades += inverteLetra(fatias)
+        
     return possibilidades
-
 
 def inserirLetra(palavras):
     novas_palavras = []
@@ -29,6 +48,30 @@ def inserirLetra(palavras):
     for E, D in palavras:
         for letra in letras:
             novas_palavras.append(E + letra + D)
-        return novas_palavras
+    return novas_palavras
 
-def corretor(palavras):
+def deletandoLetra(palavras):
+    novas_palavras = []
+    for E, D in palavras:
+        novas_palavras.append(E + D[1:])
+    return novas_palavras
+
+def trocaLetra(palavras):
+    novas_palavras = []
+    letras = 'abcdefghijklmnopqrstuvwxyzáâàãéêèẽíîìĩóôõòúûùũç'
+    for E, D in palavras:
+        for letra in letras:
+            novas_palavras.append(E + letra + D[1:])
+    return novas_palavras
+
+def inverteLetra(palavras):
+    novas_palavras = []
+    for E, D in palavras:
+        if len(D) > 1:
+            novas_palavras.append(E + D[1] + D[0] + D[2:])
+    return novas_palavras
+
+def probabilidade(palavras_gerada):
+    total_palavras = len(listaPalavras)
+    frequencia = nltk.FreqDist(listaPalavras)
+    return frequencia[palavras_gerada]/total_palavras
--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import nltk
 from correcaoOrtografica import correcao
 
 
-frase = "Oiá, tldo bem? você podeia testar a formatação e correçao de texto d nlkt."
-
+frase = "olhm, stamos tentundo corigir as palabras"
+#frase = "tldo"
 
 def main():
     correcao.principal(frase)
-
 
 if __name__ == "__main__":
     main()
